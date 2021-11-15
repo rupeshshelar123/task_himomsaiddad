5150e6623b070ebcae1bb808b2c88a1fbe8959c7
Vikramjeet Singh <vikramjeet.singh@innovationm.com>
2021-11-15 13:26:06 +0530

    loading text added

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,26 @@ const App = () => {
   let [slider, setSlider] = useState(10);
   let [bgColor, setBgColor] = useState('#fff');
   let [wtype, setWtype] = useState('');
+  let [loading, setLoading] = useState('');
   
   const getLocation = () : void => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((location) =>
-        apiCall(location.coords.latitude, location.coords.longitude)
-      );
+      setLoading('Fetching location coordinates');
+      navigator.geolocation.getCurrentPosition((location) => {
+        apiCall(location.coords.latitude, location.coords.longitude);
+        setLoading('');
+      });
     } else {
       console.error('Cannot get Location');
     }
   }
 
   const apiCall = async (lat: any, lon: any) => {
+    setLoading('Waiting for response from api');
     const url = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`;
     const req = axios.get(url);
     const res = await req;
-
+    setLoading('');
     setTemp(res.data.main.temp);
     setWtype(res.data.weather[0].description);
     setBgColor(getColor(res.data.main.temp));
@@ -59,27 +63,30 @@ const App = () => {
       <div className='weather-control'>
         <h1>Weather App</h1>
         <div>
-          <h3 onClick={getLocation}>GET WEATHER</h3>
+          <button onClick={() => getLocation()} style={{height: '30px', backgroundColor: 'lightblue', margin: '10px', borderRadius: '5px', padding: '5px' }}>GET WEATHER</button>
         </div>
-        <div>
-          <label>
-            <input
-              id='typeinp'
-              type='range'
-              min='-10'
-              max='30'
-              value={slider}
-              onChange={handleChange}
-              step='1'
-            />
-            {slider}
-          </label>
-        </div>
+        {temp && (
+          <div>
+            <label>
+              <input
+                id='typeinp'
+                type='range'
+                min='-10'
+                max='30'
+                value={slider}
+                onChange={(e) => handleChange(e)}
+                step='1'
+              />
+              {slider}
+            </label>
+          </div>
+        )}
       </div>
       <div className='weather-info'>
         <h3>WELCOME</h3>
-        <h1>{temp ? `${temp} \xB0` : `--`}</h1>
-        <h3>{temp ? wtype.toLocaleUpperCase() : `Loading...`}</h3>
+        <h1 style={{margin : '10px'}}>{temp && `${temp} \xB0`}</h1>
+        {loading?.length > 0 && <h3>{loading}</h3>}
+        <h3>{temp  && wtype.toLocaleUpperCase()}</h3>
       </div>
     </div>
   );
