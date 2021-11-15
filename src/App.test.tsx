72fd3bf2b2e7db8d8252f1f167e137655f15d62b
Vikramjeet Singh <vikramjeet.singh@innovationm.com>
2021-11-15 14:05:00 +0530

    added test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// To mock location permission and coordinates
+global.navigator.permissions = {
+  query: jest
+    .fn()
+    .mockImplementationOnce(() => Promise.resolve({ state: 'granted' })),
+};
+
+global.navigator.geolocation = {
+  getCurrentPosition: jest.fn().mockImplementationOnce((success) =>
+    Promise.resolve(
+      success({
+        coords: {
+          latitude: 43.21,
+          longitude: -87.654321,
+        },
+      })
+    )
+  ),
+};
+
+
+test('Contains Weather App as text', () => {
+  const {getByText} = render(<App />);
+  expect(getByText('Weather App')).toBeTruthy();
+});
+
+test('Contains Welcome text', () => {
+  const {getByText} = render(<App />);
+  expect(getByText('Welcome', { exact: false } )).toBeTruthy();
+});
+
+test('Displays Fetching coordinates after click on Get Weather button', () => {
+  const {getByText} = render(<App />);
+  fireEvent.click(screen.getByText('Get weather', { exact: false } ));
+  expect(getByText('Fetching location coordinates')).toBeTruthy();
 });
